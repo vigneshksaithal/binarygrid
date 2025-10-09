ffe92194893578262fa728c6bb4427a57688ed08
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-10-09 12:18:40 +0530

    style: update Button component for improved attribute formatting and readability

--- src/client/components/Button.svelte
+++ src/client/components/Button.svelte
@@ -17,13 +17,13 @@ const {
 </script>
 
 <button
-  type={type}
-  class="
+	{type}
+	class="
     inline-flex items-center justify-center w-full shrink-0 h-9 px-4 py-2 gap-2 rounded-2xl text-sm sm:text-base font-semibold shadow-md cursor-pointer bg-green-900 text-green-500 hover:bg-green-900/90
     {classes}
   "
-  onclick={onClick}
-  disabled={disabled}
+	onclick={onClick}
+	{disabled}
 >
-  {@render children?.()}
+	{@render children?.()}
 </button>
