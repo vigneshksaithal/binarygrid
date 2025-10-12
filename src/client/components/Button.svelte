bae4d93bbd2d558b10da2a449139938579c71cd5
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-10-12 20:42:48 +0530

    style: refine Button component styling by removing unused classes for cleaner markup

--- src/client/components/Button.svelte
+++ src/client/components/Button.svelte
@@ -5,13 +5,11 @@ const {
   onClick,
   disabled = false,
   type = 'button',
-  classes = '',
   children
 }: {
   onClick?: () => void
   disabled?: boolean
   type?: 'button' | 'submit' | 'reset'
-  classes?: string
   children?: Snippet
 } = $props()
 </script>
@@ -19,8 +17,7 @@ const {
 <button
 	{type}
 	class="
-    inline-flex items-center justify-center w-full shrink-0 h-9 px-4 py-2 gap-2 rounded-2xl text-sm sm:text-base font-semibold shadow-md cursor-pointer bg-green-900 text-green-500 hover:bg-green-900/90
-    {classes}
+    w-full px-4 py-2 rounded-2xl text-center text-sm sm:text-base font-semibold shadow-md cursor-pointer bg-green-900 text-green-500 hover:bg-green-900/90
   "
 	onclick={onClick}
 	{disabled}
