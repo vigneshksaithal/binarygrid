a41d2306669eabd89194ca7f600d0e646d1112d1
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-10-25 22:01:42 +0530

    refactor: change span to paragraph for better semantic structure in Timer component

--- src/client/components/Timer.svelte
+++ src/client/components/Timer.svelte
@@ -3,10 +3,10 @@ import { game } from '../stores/game'
 import { elapsedSeconds, formatElapsedTime } from '../stores/timer'
 </script>
 
-<span class="text-primary-green text-sm sm:text-base font-semibold text-center">
+<p class="text-primary-green font-semibold text-center">
 	{#if $game.status === 'idle'}
 		Time: --:--
 	{:else}
 		Time: {formatElapsedTime($elapsedSeconds)}
 	{/if}
-</span>
+</p>
