e2e60a3068518f65ff4528270f4219371aef4ea6
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-10-26 11:38:09 +0530

    fix: change <p> to <div> for consistency in Timer.svelte

--- src/client/components/Timer.svelte
+++ src/client/components/Timer.svelte
@@ -3,10 +3,10 @@ import { game } from '../stores/game'
 import { elapsedSeconds, formatElapsedTime } from '../stores/timer'
 </script>
 
-<p class="text-primary-green font-semibold text-center">
+<div class="text-primary-green font-semibold text-center">
 	{#if $game.status === 'idle'}
 		Time: --:--
 	{:else}
 		Time: {formatElapsedTime($elapsedSeconds)}
 	{/if}
-</p>
+</div>
