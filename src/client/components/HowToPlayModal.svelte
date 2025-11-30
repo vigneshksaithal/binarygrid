a87d54e2cd0940557f3c47ab11173fc5bf2b7c6f
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-11-30 22:03:24 +0530

    style(HowToPlayModal): update heading format and change list to ordered for improved clarity

--- src/client/components/HowToPlayModal.svelte
+++ src/client/components/HowToPlayModal.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { closeHowTo, showHowToModal } from '../stores/ui'
-	import Button from './Button.svelte'
 	import Modal from './Modal.svelte'
 </script>
 
@@ -10,17 +9,12 @@
 	labelledby="how-to-modal-title"
 	describedby="how-to-modal-body"
 >
-	<h2 id="how-to-modal-title">How to Play</h2>
+	<h2 id="how-to-modal-title">>HOW TO PLAY</h2>
 	<div id="how-to-modal-body">
-		<ul>
-			<li>
-				Fill the grid so every row and column has exactly 3 zeros and 3 ones.
-			</li>
+		<ol>
 			<li>Tap any cell to cycle through: empty → 0 → 1</li>
+			<li>Fill the grid so every row and column has 3 zeros and 3 ones.</li>
 			<li>No triples in any row or column, 000 or 111.</li>
-		</ul>
+		</ol>
 	</div>
-	<footer class="flex justify-end">
-		<Button onClick={closeHowTo}>Got it</Button>
-	</footer>
 </Modal>
