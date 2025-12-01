cedf4b3dbf98fb259b75875107dcbc25a84b436e
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-12-01 12:43:19 +0530

    feat(Modal): add close button with icon for improved user experience and accessibility

--- src/client/components/Modal.svelte
+++ src/client/components/Modal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import XIcon from '@lucide/svelte/icons/x'
 	import type { Snippet } from 'svelte'
 	import Button from './Button.svelte'
 
@@ -26,16 +27,18 @@
 		class="fixed inset-0 z-50 flex items-center justify-center bg-green-200 dark:bg-green-950 backdrop-blur-xs p-4 sm:p-6 overflow-y-auto"
 	>
 		<div
-			class="w-full max-w-md bg-green-100 dark:bg-green-900 p-4 sm:p-4 border-2 border-green-800 dark:border-green-600 max-h-[min(90dvh,42rem)] overflow-y-auto"
+			class="relative w-full max-w-md bg-green-100 dark:bg-green-900 p-4 sm:p-4 border-2 border-green-800 dark:border-green-600 max-h-[min(90dvh,42rem)] overflow-y-auto"
 			{role}
 			aria-modal={ariaModal}
 			aria-labelledby={labelledby}
 			aria-describedby={describedby}
 		>
+			<div class="absolute top-2 right-2">
+				<Button onClick={onClose} ariaLabel="Close" variant="ghost" size="icon-sm">
+					<XIcon class="size-4" />
+				</Button>
+			</div>
 			{@render children?.()}
-			<footer class="flex justify-end mt-4">
-				<Button onClick={onClose} ariaLabel="Close">Exit</Button>
-			</footer>
 		</div>
 	</section>
 {/if}
