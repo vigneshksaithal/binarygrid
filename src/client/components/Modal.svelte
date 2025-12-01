f7fe61a89b127f2f7e583a50040b830ad5789552
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-12-01 15:43:22 +0530

    style(Modal): update modal width and close button variant for improved layout and user experience

--- src/client/components/Modal.svelte
+++ src/client/components/Modal.svelte
@@ -27,7 +27,7 @@
 		class="fixed inset-0 z-50 flex items-center justify-center bg-green-200 dark:bg-green-950 backdrop-blur-xs p-4 sm:p-6 overflow-y-auto"
 	>
 		<div
-			class="relative w-full max-w-md bg-green-100 dark:bg-green-900 p-4 sm:p-4 border-2 border-green-800 dark:border-green-600 max-h-[min(90dvh,42rem)] overflow-y-auto"
+			class="relative w-full max-w-sm bg-green-100 dark:bg-green-900 p-4 sm:p-4 border-2 border-green-800 dark:border-green-600 max-h-[min(90dvh,42rem)] overflow-y-auto"
 			{role}
 			aria-modal={ariaModal}
 			aria-labelledby={labelledby}
@@ -35,10 +35,10 @@
 		>
 			<div class="absolute top-4 right-4">
 				<Button
+					variant="ghost"
+					size="icon-sm"
 					onClick={onClose}
 					ariaLabel="Close"
-					variant="default"
-					size="icon-sm"
 				>
 					<XIcon class="size-6" />
 				</Button>
