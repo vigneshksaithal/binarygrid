55eccc86989b2ccb045f649c7c3e78929f9dda50
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-12-02 13:44:30 +0530

    style(app.css, Cell, Modal): update background colors for improved visual consistency and clarity

--- src/client/components/Cell.svelte
+++ src/client/components/Cell.svelte
@@ -26,8 +26,8 @@
 
 <button
 	type="button"
-	class="aspect-square font-mono font-extrabold text-2xl text-green-800 dark:text-green-400 focus:outline-none border-2 border-green-500 dark:border-green-700 focus:ring-2 focus:ring-green-500 disabled:cursor-not-allowed transition-colors hover:bg-green-500/10 dark:hover:bg-green-600/10 relative {fixed
-		? 'bg-green-400 dark:bg-green-800 text-green-600 dark:text-green-400'
+	class="aspect-square font-mono font-extrabold text-2xl text-green-600 dark:text-green-400 focus:outline-none border-2 border-green-500 dark:border-green-700 focus:ring-2 focus:ring-green-500 disabled:cursor-not-allowed transition-colors hover:bg-green-500/10 dark:hover:bg-green-600/10 relative {fixed
+		? 'bg-green-200 dark:bg-green-800'
 		: ''} {borderClass}"
 	onclick={handleClick}
 	aria-label={label}
