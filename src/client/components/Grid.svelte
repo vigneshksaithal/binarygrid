cf441877c9f6c1e3fc32ae7b3314032dfc7b4ab8
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-12-15 21:24:12 +0530

    style: adjust grid gap in Grid.svelte for improved layout
    - Increased the gap in the grid from 0.5 to 1, enhancing the visual spacing of grid items for better clarity.

--- src/client/components/Grid.svelte
+++ src/client/components/Grid.svelte
@@ -14,7 +14,7 @@
 	const errorColSet = $derived(new Set($game.errorLocations?.columns ?? []))
 </script>
 
-<div class="w-full grid grid-cols-6 gap-0.5">
+<div class="w-full grid grid-cols-6 gap-1">
 	{#if $game.status === 'solved'}
 		<p class="col-span-6 text-center">Solved</p>
 	{/if}
