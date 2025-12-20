9587583c3a86daa3186319eb45d56b422600efdb
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-12-20 16:35:53 +0530

    refactor: simplify modal rendering in App component
    - Removed lazy loading for `HowToPlayModal` and `SuccessModal`, directly importing and rendering them for improved code clarity.
    - Streamlined the App component by eliminating unnecessary conditional rendering, enhancing maintainability and readability.

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -4,16 +4,13 @@
 	import './app.css'
 	import Button from './components/Button.svelte'
 	import Grid from './components/Grid.svelte'
+	import HowToPlayModal from './components/HowToPlayModal.svelte'
 	import PlayOverlay from './components/PlayOverlay.svelte'
+	import SuccessModal from './components/SuccessModal.svelte'
 	import Timer from './components/Timer.svelte'
 	import { game, undo, useHint } from './stores/game'
 	import { canUseHint, cooldownProgress } from './stores/hint'
-	import {
-		openHowToModal,
-		openPlayOverlay,
-		showHowToModal,
-		showSuccessModal,
-	} from './stores/ui'
+	import { openHowToModal, openPlayOverlay } from './stores/ui'
 
 	const handleHint = () => {
 		if ($canUseHint) {
@@ -104,14 +101,5 @@
 
 <PlayOverlay />
 
-{#if $showHowToModal}
-	{#await import('./components/HowToPlayModal.svelte') then { default: HowToPlayModal }}
-		<HowToPlayModal />
-	{/await}
-{/if}
-
-{#if $showSuccessModal}
-	{#await import('./components/SuccessModal.svelte') then { default: SuccessModal }}
-		<SuccessModal />
-	{/await}
-{/if}
+<HowToPlayModal />
+<SuccessModal />
